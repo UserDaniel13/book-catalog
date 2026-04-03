eb5ac59e423de2737d884dbcdfd1f723ced2e92a
UserDaniel <kim-danil2002@mail.ru>
2026-04-03 23:35:15 +0300

    Добавление подсчёта количества и стоимости книг

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,14 @@
 function Cart({ cart }) {
+    const totalPrice = cart.reduce((sum, item) => sum + item.price, 0);
     return (
         <div>
             <h2>Корзина</h2>
             {cart.map((item, index) => (
                 <p key={index}>{item.title}</p>
             ))}
+
+            <h3>Количество книг: {cart.length}</h3>
+            <h3>Общая сумма: {totalPrice} руб.</h3>
         </div>
     );
 }
